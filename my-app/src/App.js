ee41f1cb0e813cded8bff8d37dc152a5cb5ae294
Shubhavee <sshubhavee00@gmail.com>
2020-02-06 00:30:23 +0530

    crash_course_part1

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import {Component} from 'react';
+import React from 'react';
+import Todos from './components/Todos';
+import './App.css';
+
+class App extends Component{
+  state = {
+    todos: [
+      {
+        id:1,
+        title: 'Take out trash',
+        completed: false
+      },
+      {
+        id:2,
+        title: 'Dinner with wife',
+        completed: false
+      },
+      {
+        id:3,
+        title: 'Meeting with boss',
+        completed: false
+      }
+    ]
+  }
+
+  render(){
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
+}
+
+export default App;
