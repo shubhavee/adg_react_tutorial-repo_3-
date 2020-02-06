a1754bd579ecb8180d7b196373d4f2e5ed6a6669
Shubhavee <sshubhavee00@gmail.com>
2020-02-06 23:12:03 +0530

    crash course video

---
+++ my-app/src/components/TodoItem.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types'
+
+// import React from 'react';
+
+export class TodoItem extends Component {
+  getStyle=()=>{
+    return{
+    background:'#f4f4f4',
+    padding:'10px',
+    borderBottom: '1px #ccc dotted',
+    textDecoration: this.props.todo.completed?'line-through':'none'
+  }
+    }
+  markComplete=(e)=>{
+    console.log(this.props)
+  }
+
+  render(){
+  return(
+    <div style={this.getStyle()}>
+    <p>
+    <input type="checkbox" onChange={this.markComplete}/> {' '}
+    {this.props.todo.title}
+    </p>
+    </div>
+  )
+  }
+}
+  TodoItem.propTypes={
+    todo:PropTypes.object.isRequired
+  }
+
+export default TodoItem;
--- my-app/src/components/Todos.js
+++ my-app/src/components/Todos.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
+// import React from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types'
 
-function Todos() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+class Todos extends Component {
+  render(){
+    // console.log(this.props.todos)
+    return this.props.todos.map((todo)=>(
+      // <h3>{todo.title}</h3>
+      <TodoItem key={todo.id} todo={todo}/>
+    ));
+  }
 }
+
+  Todos.propTypes={
+    todos:PropTypes.array.isRequired
+  }
 
 export default Todos;
