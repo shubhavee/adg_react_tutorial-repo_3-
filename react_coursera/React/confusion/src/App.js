785493730c7ff1a1b85ea15315f4e67c6377c6fe
Shubhavee <sshubhavee00@gmail.com>
2020-03-28 21:06:42 +0530

    components part 2

--- react_coursera/React/confusion/src/App.js
+++ react_coursera/React/confusion/src/App.js
@@ -3,8 +3,19 @@ import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './shared/dishes';
+import {Component} from 'react';
 
-function App() {
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      dishes:DISHES
+    };
+  }
+
+  render(){
   return (
     // <div className="App">
     <div>
@@ -13,9 +24,10 @@ function App() {
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
-      <Menu></Menu>
+      <Menu dishes={this.state.dishes}></Menu>
     </div>
   );
+}
 }
 
 export default App;
